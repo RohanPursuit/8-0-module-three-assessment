4c780385cc580e0cc0696af2182bb105a4fe9dc3
Rohan-Sterling <rohansterling@pursuit.org>
2021-12-16 10:30:32 -0500

    add People.js with class component

---
+++ src/components/People.js
@@ -0,0 +1,17 @@
+import {Component} from 'react'
+import "./People.css";
+
+class People extends Component {
+
+  render() {
+    return (
+      <div className="People">
+        <main>
+          <h1>Hello, world!</h1>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default People;
