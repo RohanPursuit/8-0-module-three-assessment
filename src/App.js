35ecf1f51aefb1f6790cfa5f3c568483d0bc1035
Rohan-Sterling <rohansterling@pursuit.org>
2021-12-16 10:34:02 -0500

    add css files for Locations, Movies, People and Navbar components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {Component} from 'react'
 import "./App.css";
+import Navbar from './components/common/Navbar'
 
 class App extends Component {
 
   render() {
     return (
       <div className="app">
+        <Navbar/>
         <main>
           <h1>Hello, world!</h1>
         </main>
