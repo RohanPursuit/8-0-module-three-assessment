a7839bd92135e052ac5cdd4bf5a694ea1ed7acc7
Rohan-Sterling <rohansterling@pursuit.org>
2021-12-16 10:29:54 -0500

    add Navbar.js with class component

---
+++ src/components/common/Navbar.js
@@ -0,0 +1,17 @@
+import {Component} from 'react'
+import "./Navbar.css";
+
+class Navbar extends Component {
+
+  render() {
+    return (
+      <div className="app">
+        <main>
+          <h1>Hello, world!</h1>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
