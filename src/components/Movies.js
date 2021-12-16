69e430048a9780b0664ec72bd4111418bd2d6253
Rohan-Sterling <rohansterling@pursuit.org>
2021-12-16 10:30:19 -0500

    add Movies.js with class component

---
+++ src/components/Movies.js
@@ -0,0 +1,17 @@
+import {Component} from 'react'
+import "./Movies.css";
+
+class Movies extends Component {
+
+  render() {
+    return (
+      <div className="Movies">
+        <main>
+          <h1>Hello, world!</h1>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Movies;
