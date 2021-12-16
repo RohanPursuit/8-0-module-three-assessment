d6107937c5b84901ce7627d8d8bbad8c4e45d794
Rohan-Sterling <rohansterling@pursuit.org>
2021-12-16 11:14:24 -0500

    add img tag with height and width of 50px

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -7,7 +7,9 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="app">
-        <Link to="/">Home</Link>
+        <Link to="/">
+            <img src="#" alt=""/>
+        </Link>
         <Link to="/movies">Movies</Link>
         <Link to="/people">People</Link>
         <Link to="/locations">Locations</Link>
