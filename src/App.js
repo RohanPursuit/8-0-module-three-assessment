1f9c7d84c2c99cce3f9883e6058e468737e4453d
Rohan-Sterling <rohansterling@pursuit.org>
2021-12-16 10:29:35 -0500

    add class component to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import {Component} from 'react'
 import "./App.css";
 
-function App() {
-  return (
-    <div className="app">
-      <main>
-        <h1>Hello, world!</h1>
-      </main>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="app">
+        <main>
+          <h1>Hello, world!</h1>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
