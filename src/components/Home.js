57c1cd1f57d50b5fff26d3175db5e1ac23eadd5b
Rohan-Sterling <rohansterling@pursuit.org>
2021-12-16 10:41:02 -0500

    add Home.css and Home.css

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import {Component} from 'react'
+import "./Home.css";
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div className="Home">
+        <main>
+          <h1>Hello, world!</h1>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Home;
